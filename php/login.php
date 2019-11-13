9818210c2b2961187307c925f9dfd7b94c924730
vsaisujeeth <vsaisujeeth10@yahoo.com>
2019-11-13 19:28:21 +0530

    added login

--- php/login.php
+++ php/login.php
@@ -6,21 +6,28 @@ if ( ! empty( $_POST ) ) {
     if ( isset( $_POST['username'] ) && isset( $_POST['password'] ) ) {
         // Getting submitted user data from database
         $con = new mysqli("localhost", "root", "", "dbms");
-        $stmt = $con->prepare("SELECT * FROM admin WHERE username = ?");
+        $stmt = $con->prepare("SELECT * FROM admin WHERE faculty_id = ?");
         $stmt->bind_param('s', $_POST['username']);
         $stmt->execute();
         $result = $stmt->get_result();
     	$user = $result->fetch_object();
     		
-    	// Verify user password and set $_SESSION
-    	if ( password_verify( $_POST['password'], $user->password ) ) {
-            $_SESSION['user_id'] = $user->ID;
-            if()
+        // Verify user password and set $_SESSION
+        // echo($_POST['password']);
+        // echo($user->password);
+    	if ($_POST['password'] === $user->password ) {
+            $_SESSION['user_id'] = $user->faculty_id;
+            // echo("shit");
+            if($user->faculty_id == "admin")
             {
-                GOTO();
+                header("Location: ../admin.html");
             }
+            else{
+                header("Location: ../profile.html");
+            }
+           
     	}else{
-            echo("<h1>wrong password or username<h1>");
+            echo("<h5>wrong password or username<h5>");
         }
     }
 }
